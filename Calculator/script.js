cb8b2bb813ba9af510bf01b6ea112e7bab2f2850
Bipin Thapa <yukichin638@gmail.com>
2024-10-20 21:56:10 +0545

    Day 2

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,9 +1,10 @@
 let displayContent = '';
+let calculate = '';
 let calcStatus = 'off';
 
 if(calcStatus === 'off')
 {
-    document.querySelector('.note').innerText = `Note: The Calculator is ${calcStatus}`;
+    document.querySelector('.top4').innerText = `The Calculator is ${calcStatus}`;
 }
 
 function power(a)
@@ -11,13 +12,18 @@ function power(a)
     if(a === 'on')
     {
         calcStatus = a;
-        document.querySelector('.note').innerText = `Note: The Calculator is ${calcStatus}`;
+        document.querySelector('.top4').innerText = `The Calculator is ${calcStatus}`;
+        document.querySelector('.top4').style.color = 'green';
     }
     if(a === 'off')
     {
         calcStatus = a;
-        document.querySelector('.note').innerText = `Note: The Calculator is ${calcStatus}`;
-        document.querySelector('.display').innerText = '';
+        document.querySelector('.top4').innerText = `The Calculator is ${calcStatus}`;
+        document.querySelector('.top4').style.color = 'red';
+        document.querySelector('.display1').innerText = '';
+        document.querySelector('.display2').innerText = '';
+        displayContent = '';
+        calculate = '';
     }
 }
 
@@ -28,23 +34,42 @@ function button(a)
     {
         if(a === '=')
         {
-            document.querySelector('.display').innerText = eval(displayContent);
+            document.querySelector('.display2').innerText = eval(calculate);
+            console.log(eval(calculate));
             return;
         }
         if(a === 'del')
         {
             displayContent = displayContent.slice(0,-1);
-            document.querySelector('.display').innerText = displayContent;
+            calculate = calculate.slice(0,-1);
+            document.querySelector('.display1').innerText = displayContent;
             return;
         }
         if(a === 'ac')
         {
             displayContent = '';
-            document.querySelector('.display').innerText = displayContent;
+            calculate = '';
+            document.querySelector('.display1').innerText = displayContent;
+            document.querySelector('.display2').innerText = calculate;
+            return;
+        }
+        if(a === '*10')
+        {
+            displayContent += '*10^';
+            calculate += '*10**';
+            document.querySelector('.display1').innerText = displayContent;
+            return;
+        }
+        if(a === '^')
+        {
+            displayContent += a;
+            calculate += '**';
+            document.querySelector('.display1').innerText = displayContent;
             return;
         }
         displayContent += a;
-        document.querySelector('.display').innerText = displayContent;
+        calculate += a;
+        document.querySelector('.display1').innerText = displayContent;
     }
     
 }
