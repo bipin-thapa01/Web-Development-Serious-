2fa6064727059290f879e966fd10a69df84d3097
Bipin Thapa <yukichin638@gmail.com>
2024-10-24 22:01:04 +0545

    Day 6

--- To Do List/script.js
+++ To Do List/script.js
@@ -1,55 +1,55 @@
+let nonCompleteTask = [];
+let completedTask = [];
+
 function mainFunction() {
-    for (let i = 0; i < localStorage.length; i++) {
-        let task = JSON.parse(localStorage.getItem(`task${i + 1}`));
-        console.log(task);
-        if(task === null)
-        {
-            continue;
-        }
-        if (task.complete_status === 'no') {
-            let add_location = document.querySelector('.tasks');
-            let li = document.createElement('li');
-            li.classList.add(`number${i + 1}`);
-            let div = document.createElement('div');
-            div.innerText = task.task;
-            li.appendChild(div);
-            {
-                let div = document.createElement('div');
-                let needed_time = task.time_of_addition;
-                div.classList.add('time');
-                div.innerText = needed_time;
-                li.appendChild(div);
-            }
-            {
-                let div = document.createElement('div');
-                let done_button = document.createElement('button');
-                done_button.classList.add('done');
-                {
-                    let i = document.createElement('i');
-                    i.classList.add('fa-solid', 'fa-check');
-                    done_button.appendChild(i);
-                }
-                done_button.onclick = function () {
-                    done(li.className);
-                };
-                div.appendChild(done_button);
-                let del_button = document.createElement('button');
-                del_button.classList.add('delete-item');
-                {
-                    let i = document.createElement('i');
-                    i.classList.add('fa-solid', 'fa-trash');
-                    del_button.appendChild(i);
-                }
-                del_button.onclick = function () {
-                    del(li.className);
-                };
-                div.appendChild(del_button);
-                div.classList.add('item-option');
-                li.appendChild(div);
-            }
-            add_location.appendChild(li);
-        }
+    nonCompleteTask = JSON.parse(localStorage.getItem('nonComplete'));
+    console.log(nonCompleteTask)
+    if(nonCompleteTask !== null)
+    {
+        nonCompleteTask.forEach(input => {
+            addElement(input);
+        })
     }
+}
+
+function addElement(input) {
+    const add_location = document.querySelector('.tasks');
+    let li = document.createElement('li');
+    li.classList.add(input.class);
+    //main text append
+    {
+        let div = document.createElement('div');
+        div.innerText = input.goal;
+        li.appendChild(div);
+    }
+    //time append
+    {
+        let div = document.createElement('div');
+        div.innerText = input.t;
+        div.classList.add('time');
+        li.appendChild(div);
+    }
+    //done and delete append
+    {
+        let div = document.createElement('div');
+        div.classList.add('item-option');
+        //done button
+        let doneButton = document.createElement('button');
+        doneButton.classList.add('fa-solid', 'fa-check', 'done');
+        doneButton.addEventListener('click', event => {
+            done(input);
+        })
+        div.append(doneButton);
+        //delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('fa-solid', 'fa-trash', 'delete-item');
+        deleteButton.addEventListener('click', event => {
+            del(input);
+        })
+        div.append(deleteButton);
+        li.appendChild(div);
+    }
+    add_location.appendChild(li);
 }
 
 function changeColor() {
@@ -99,129 +99,61 @@ function changeColor() {
 
 function add() {
     const task = document.querySelector('#task').value;
-    const add_location = document.querySelector('.tasks')
+    const add_location = document.querySelector('.tasks');
     if (task === '') {
         alert("You must write something to add it as task!");
     }
     else {
-        const time = new Date();
-        let tasks = {
-            task: task,
-            time_of_addition: time.toDateString(),
-            complete_status: 'no',
+        let time = new Date();
+        time = time.toDateString();
+        let classValue = add_location.childElementCount;
+        let input = {
+            goal: task,
+            t: time,
+            class: `task${classValue + 1}`,
         };
-        localStorage.setItem(`task${localStorage.length + 1}`, JSON.stringify(tasks));
-        let li = document.createElement('li');
-        li.classList.add(`number${localStorage.length}`);
-        let div = document.createElement('div');
-        div.innerText = task;
-        li.appendChild(div);
-        {
-            let string_time = time.toDateString();
-            let div = document.createElement('div');
-            let needed_time = string_time;
-            div.classList.add('time');
-            div.innerText = needed_time;
-            li.appendChild(div);
+        if (nonCompleteTask === null) {
+            nonCompleteTask = [input];
         }
-        {
-            let div = document.createElement('div');
-            let done_button = document.createElement('button');
-            done_button.classList.add('done');
-            {
-                let i = document.createElement('i');
-                i.classList.add('fa-solid', 'fa-check');
-                done_button.appendChild(i);
-            }
-            done_button.classList.add(`task${localStorage.length}`);
-            done_button.onclick = function () {
-                done(li.className);
-            };
-            div.appendChild(done_button);
-            let del_button = document.createElement('button');
-            del_button.classList.add('delete-item');
-            {
-                let i = document.createElement('i');
-                i.classList.add('fa-solid', 'fa-trash');
-                del_button.appendChild(i);
-            }
-            del_button.onclick = function () {
-                del(`task${localStorage.length}`);
-            };
-            div.appendChild(del_button);
-            div.classList.add('item-option');
-            li.appendChild(div);
+        else {
+            nonCompleteTask.push(input);
         }
-        add_location.appendChild(li);
-        document.querySelector('#task').value = '';
+
+        localStorage.setItem('nonComplete', JSON.stringify(nonCompleteTask));
+        addElement(input);
     }
 }
 
 function done(item) {
-    let task = JSON.parse(localStorage.getItem(`task${item.slice(-1)}`));
-    task.complete_status = 'yes';
-    document.querySelector(`.${item}`).remove();
-    localStorage.removeItem(`task${item.slice(-1)}`);
-    localStorage.setItem(`task${item.slice(-1)}`, JSON.stringify(task));
+    // console.log(item);
+    let input = JSON.parse(localStorage.getItem('nonComplete'));
+    let completedTask = JSON.parse(localStorage.getItem('Complete'));
+    input.forEach(
+        (i, index) => {
+            if (i.class === item.class) {
+                document.querySelector(`.${i.class}`).remove();
+                if (completedTask === null) {
+                    completedTask = [i];
+                    console.log(completedTask);
+                }
+                else {
+                    completedTask.push(i);
+                }
+                input.splice(index, 1);
+                localStorage.removeItem('nonComplete');
+                localStorage.setItem('nonComplete', JSON.stringify(input));
+                localStorage.setItem('Complete', JSON.stringify(completedTask));
+            }
+        }
+    )
 }
 
 function del(item) {
-    document.querySelector(`.${item}`).remove();
-    localStorage.removeItem(`item$${item.slice(-1)}`);
-    for(let i=parseInt(item.slice(-1))+1;i<=localStorage.length;i++)
-    {
-        console.log(hi);
-        let item = JSON.parse(localStorage.getItem(`task${i}`));
-        localStorage.removeItem(`task${i}`);
-        localStorage.setItem(`task${i-1}`,JSON.stringify(item));
-    }
+
 }
 
 function show_history() {
-    if (document.querySelector('.option1').innerText === 'Tasks') {
-        return;
-    }
-    document.querySelector('.tasks').innerHTML = '';
-    if (document.querySelector('.option1').innerText === 'History') {
-        document.querySelector('.option1').innerText = 'Tasks';
-        for (let i = 0; i < localStorage.length; i++) {
-            let task = JSON.parse(localStorage.getItem(`task${i + 1}`));
-            console.log(task);
-            if (task.complete_status === 'yes') {
-                console.log('hi');
-                let add_location = document.querySelector('.tasks');
-                let li = document.createElement('li');
-                li.classList.add(`number${i + 1}`);
-                let div = document.createElement('div');
-                div.innerText = task.task;
-                li.appendChild(div);
-                {
-                    let div = document.createElement('div');
-                    let needed_time = task.time_of_addition;
-                    div.classList.add('time');
-                    div.innerText = needed_time;
-                    li.appendChild(div);
-                }
-                {
-                    let div = document.createElement('div');
-                    let del_button = document.createElement('button');
-                    del_button.classList.add('delete-item');
-                    {
-                        let i = document.createElement('i');
-                        i.classList.add('fa-solid', 'fa-trash');
-                        del_button.appendChild(i);
-                    }
-                    del_button.onclick = function () {
-                        del(`task${i + 1}`);
-                    };
-                    div.appendChild(del_button);
-                    div.classList.add('item-option');
-                    li.appendChild(div);
-                }
-                add_location.appendChild(li);
-            }
-        }
-    }
+
 }
 
 mainFunction();
