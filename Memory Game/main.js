a7b5077692dea8e719460139721635054130c096
Bipin Thapa <yukichin638@gmail.com>
2024-10-28 21:38:49 +0545

    Day 9

---
+++ Memory Game/main.js
@@ -0,0 +1,17 @@
+document.querySelector('.easy').addEventListener('click',
+event =>
+{
+  window.location.href = "easy_mode.html";
+});
+
+document.querySelector('.medium').addEventListener('click',
+event =>
+{
+  window.location.href = "medium_mode.html";
+});
+
+document.querySelector('.hard').addEventListener('click',
+event =>
+{
+  window.location.href = "hard_mode.html";
+});
--- Memory Game/script.js
+++ Memory Game/script.js
@@ -1,21 +1,69 @@
-let value = document.querySelector('.cards');
-let count = 0;
+// const { start } = require("@popperjs/core");
 
-value.addEventListener('click',event =>
+// let value = document.querySelector('.cards');
+// let count = 0;
+
+// value.addEventListener('click', event => {
+//   if (count % 2 == 0) {
+//     console.log(count);
+//     value.classList.remove('cards-rotate-again');
+//     value.classList.add('cards-rotate');
+//     count++;
+//   }
+//   else {
+//     console.log(count);
+//     value.classList.remove('cards-rotate');
+//     value.classList.remove('cards-rotate-again');
+//     count++;
+//   }
+// });
+
+let playStatus = 'no';
+
+function startGameTimer(second = 0, minute = 0, hour = 0, checkLoop = 0) {
+  if(checkLoop === 1)
+  {
+    playStatus = 'no';
+  }
+  if(playStatus === 'yes')
+  {
+    alert(`You are already playing the game!
+If you want to play new game, exit or complete the current one.`);
+    return;
+  }
+  playStatus = 'yes';
+  document.querySelector('.timer').innerText = '';
+  if (second < 10) {
+    second = `0${second}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  document.querySelector('.timer').innerText = `${hour} : ${minute} : ${second}`;
+  second = parseInt(second);
+  minute = parseInt(minute);
+  hour = parseInt(hour);
+  setTimeout(() => {
+    second++;
+    if (second > 59) {
+      second -= 60;
+      minute++;
+    }
+    if (minute > 59) {
+      minute -= 60;
+      hour++;
+    }
+    if (hour > 3) {
+      return;
+    }
+    startGameTimer(second, minute, hour, 1);
+  }, 1000);
+}
+
+document.querySelector('.back').addEventListener('click', event =>
 {
-  if(count%2==0)
-  {
-    console.log(count);
-    value.classList.remove('cards-rotate-again');
-    value.classList.add('cards-rotate');
-    count++;
-  }
-  else
-  {
-    console.log(count);
-    value.classList.remove('cards-rotate');
-    value.classList.remove('cards-rotate-again');
-    count++;
-  }
+  window.location.href = 'index.html';
 });
-
