c2b9d776deab226c1ead03659374a4c5cef0467c
Bipin Thapa <yukichin638@gmail.com>
2024-11-04 19:27:43 +0545

    Day 16

--- Password Generator/dict/output.js
+++ Password Generator/dict/output.js
@@ -1,9 +1,15 @@
-function generatePassword()
+generatePassword = () =>
 {
-  let passLength = document.querySelector('.pass-length').innerText;
-  console.log('hi');
-  console.log(passLength);
+  let passLength = document.querySelector('.pass-length').value;
+  for(let i = 0; i<passLength; i++)
+  {
+    let charChoice = Math.ceil(Math.random() * 3);
+    if(charChoice == 1)
+    {
+      
+    }
+  }
 }
 
 //for generating password
-document.querySelector('.generate').addEventListener('click', generatePassword());
+document.querySelector('#generate').addEventListener('click', generatePassword);
