8b0a99ee6d69cb38e8b8d38286e8d571d87c9829
Bipin Thapa <yukichin638@gmail.com>
2024-11-15 21:50:17 +0545

    Day 26

---
+++ Shop/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dict/*.{html,js}'],
+  theme: {
+    extend: {
+      width: {
+        '85' : '360px'
+      }
+    },
+  },
+  plugins: [],
+}
+
