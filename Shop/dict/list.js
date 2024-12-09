fb6f5f10f852851329addebc1c91fa10555eaf03
Bipin Thapa <yukichin638@gmail.com>
2024-12-09 21:39:29 +0545

    Day 52

--- Shop/dict/list.js
+++ Shop/dict/list.js
@@ -1,16 +1,3 @@
-let leftContainer = document.createElement('div');
-leftContainer.classList.add('left','dark:bg-gray-950','bg-gray-500','hover:bg-gray-950','dark:hover:bg-gray-500','hover:text-white');
-let left = document.createElement('i');
-left.classList.add('fa-solid','fa-chevron-left','left-arrow');
-document.querySelector('.button-container').appendChild(leftContainer);
-document.querySelector('.left').appendChild(left);
-
-let rightContainer = document.createElement('div');
-rightContainer.classList.add('right','dark:bg-gray-950','bg-gray-500','hover:bg-gray-950','dark:hover:bg-gray-500','hover:text-white');
-let right = document.createElement('i');
-right.classList.add('fa-solid','fa-chevron-right');
-document.querySelector('.button-container').appendChild(rightContainer);
-document.querySelector('.right').appendChild(right);
 
 let manga = ['berserk','blackclover','bleach','bluelock','dandadan','demonslayer','jjk','katekyo','naruto','onepiece','sakamotodays','tokyoghoul'];
 
@@ -25,22 +12,3 @@ function random(a){
 }
 
 manga = random(manga);
-manga.forEach((i,index) =>
-{
-  let div = document.createElement('div');
-  let img = document.createElement('img');
-  img.src = `./websrc/Items/${i}.jpg`;
-  img.classList.add('image');
-  div.appendChild(img);
-  div.classList.add('list-custom',`list-custom${index+1}`);
-  div.style.overflow = 'hidden';
-  document.querySelector('#cat1-content').appendChild(div);
-})k holslideLeft = () =>{
-  if(leftRem>0){
-    document.querySelector(`.list-custom${leftRem}`).style.display = 'block';
-    leftRem--;
-  }
-}
-
-let leftSlide = document.querySelector('.left').addEventListener('click',slideLeft);
-let rightSlide = document.querySelector('.right').addEventListener('click',slideRight);
